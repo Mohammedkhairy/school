f671a3e09f226bbf9ab5645f8bd52e5faa3a9e00
Mohammedkhairy <red.devile2011@gmail.com>
2017-06-23 13:36:58 +0200

    Revert "initial commit index.php"
    This reverts commit 0e421170c4076c11a23ec221562f6566402f55c4.

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <table>
-        <form action="action.php" method="post">
-            <tr><td>ID: </td><td> <input type="text" name="NO" /></td></tr>
-        <tr><td>Name:</td><td> <input type="text" name="NAME" /></td></tr>
-        <tr><td>Salary: </td><td><input type="text" name="SALARY" /><br></td></tr>
-            
-        <tr><td><input type="submit" value="add" /></td></tr> 
-        </form>
-        </table>
-        To see our employees just click <a href="read.php">HERE </a> <br>
-         To search for an employee just click <a href="search.php">search </a>
-    </body>
-</html>
+<?php
+
+define("HOME_PAGE", 'HomePage');
+require_once './config.php';
+router::loadcontroller();
+?>
